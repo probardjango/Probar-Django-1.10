08cc78b9e34455042c8013e466cf3cc6af6b1cb4
Karlita K <Karlita K>
2016-11-01 20:55:01 +0100

    css custom

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -7,7 +7,7 @@ from .models import Registrado
 
 # Create your views here.
 def inicio(request):
-	titulo = "HOLA"
+	titulo = "Bienvenidos."
 	if request.user.is_authenticated():
 		titulo = "Bienvenido %s" %(request.user)
 	form = RegModelForm(request.POST or None)
