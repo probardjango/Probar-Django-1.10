bc0b7acac4a741a3959e164c6e552b58602c7b89
Karlita K <Karlita K>
2016-11-02 15:38:22 +0100

    Django Registration Redux FINAL

--- src/pd110/settings.py
+++ src/pd110/settings.py
@@ -47,10 +47,12 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #apps terceros
     'crispy_forms',
+    'registration',
     #mis apps
     'boletin',
 ]
@@ -146,5 +148,8 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_env", "static_root
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_env", "media_root")
 
 
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
+SITE_ID = 1
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
--- src/pd110/urls.py
+++ src/pd110/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -27,6 +27,7 @@ urlpatterns = [
     url(r'^contact/$', views.contact, name='contact'),
     url(r'^about/$', about, name='about'),
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('registration.backends.default.urls')),
 
 ]
 
